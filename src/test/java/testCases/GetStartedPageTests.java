9889b9b6a8bc866902586690138f51b7c72cc47b
vijayashree <vijayashree80sv@gmail.com>
2024-08-22 08:02:27 -0500

    Viji changes

--- src/test/java/testCases/GetStartedPageTests.java
+++ src/test/java/testCases/GetStartedPageTests.java
@@ -11,13 +11,13 @@ import pageObjects.HomePage;
 public class GetStartedPageTests extends BaseClass {
 	
 	Logger logger = BaseClass.getLogger();
-	
+	GetStartedPage sp = new GetStartedPage(BaseClass.getDriver());
+	HomePage hp = new HomePage(BaseClass.getDriver());
+		
 	@Test (priority=1)
 	public void TestGetStartedButton()  {
 		logger.info("Clicking the GetStarted Button.............");
-		GetStartedPage sp = new GetStartedPage(BaseClass.getDriver());
 		sp.clickGetStartedSP();
-		HomePage hp = new HomePage(BaseClass.getDriver());
 		boolean status = hp.NumpyNinjaLinkDisplay();
 		Assert.assertEquals(status, true ,"Home page is not displayed");	
 		logger.info("Home page is displayed");
@@ -27,7 +27,7 @@ public class GetStartedPageTests extends BaseClass {
 	@Test(priority=2)
 	public void CopyrightNumpyNinja2021Display(){
 		logger.info("Check the display of Copyright@NumpyNinja2021............");
-		GetStartedPage sp = new GetStartedPage(BaseClass.getDriver());
+		sp = new GetStartedPage(BaseClass.getDriver());
 		boolean status = sp.copyrightDisplay();
 		Assert.assertTrue(status); 
 		logger.info("Copyright@NumpyNinja2021 is displayed");
