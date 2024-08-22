39547f87253db444acb98fd47fedde25802f7718
vijayashree <vijayashree80sv@gmail.com>
2024-08-22 10:21:23 -0500

    comment add

--- src/test/java/testCases/SignInPageTests.java
+++ src/test/java/testCases/SignInPageTests.java
@@ -1,5 +1,22 @@
 package testCases;
 
-public class SignInPageTests {
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
 
+import factory.BaseClass;
+import pageObjects.SignInPage;
+
+public class SignInPageTests {
+	//--Comment
+	Logger logger = BaseClass.getLogger();
+	SignInPage sp = new SignInPage(BaseClass.getDriver());		
+	
+	@Test
+	public void checkValidAndInvalidLoginTest(String user, String ) {
+		logger.info("Enter the username at login page.............");
+		 sp.enterUsername(user);
+		 sp.enterpassword(pwd);
+		 sp.clickLogin();
+		 
+	}
 }
